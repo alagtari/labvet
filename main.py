238ff9b844d346c126112b8cef03c88f03b4228f
alagtari <alagtari@yahoo.fr>
2022-02-28 12:03:37 +0100

    test

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 from fastapi import FastAPI
-import uvicorn
-import  models
 from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
-from database import  engine
-from  routes import auth,user,famile,nature,methode,echantillon,client,demande,association,statistique,parametre,departement
 
 
-
-
-models.Base.metadata.create_all(bind=engine)   
-    
 
 
 origins = ["*"]
@@ -29,20 +21,3 @@ app.add_middleware(
 @app.get('/')
 def home():
     return 'hello world'
-
-app.include_router(auth.router)
-app.include_router(user.router)
-app.include_router(famile.router)
-app.include_router(nature.router)
-app.include_router(methode.router)
-app.include_router(parametre.router)
-app.include_router(echantillon.router)
-app.include_router(client.router)
-app.include_router(demande.router)
-app.include_router(parametre.router)
-app.include_router(departement.router)
-app.include_router(association.router)
-app.include_router(statistique.router)
-
-if __name__ == "__main__":
-    uvicorn.run(app, port=5000)
