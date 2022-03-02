7367b2569be2716930ad1323cc90841ee0666ad7
alagtari <62443387+alagtari@users.noreply.github.com>
2022-03-02 23:51:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,3 @@ app.include_router(departement.router)
 app.include_router(association.router)
 app.include_router(statistique.router)
 
-import uvicorn
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
